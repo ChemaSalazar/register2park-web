9c340f73c1dde0440f89ba363d0a0bf1f2540c81
Chema Salazar <chemasalazar@outlook.com>
2024-01-22 22:36:26 -0600

    added bootstrap & some styling changes for nav, global, and content sections

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import {NavbarComponent} from "./navbar/navbar.component";
 import {RouterLink, RouterOutlet} from '@angular/router';
+import {  } from 'ngx-bootstrap/modal';
+
 
 @Component({
   selector: 'app-root',
